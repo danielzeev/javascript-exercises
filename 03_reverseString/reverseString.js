0c92c08ba56f8b63dbc2334fc6ff19ce2f95729d
Daniel Zeev <86726359+danielzeev@users.noreply.github.com>
2024-08-15 15:58:13 +0300

    Added solution to exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    word = word.toString();
+    let reversedWord = "";
+    for (i = word.length - 1; i >= 0; i--) {
+        reversedWord += word[i];
+    }    
+    return reversedWord;
 };
 
 // Do not edit below this line
