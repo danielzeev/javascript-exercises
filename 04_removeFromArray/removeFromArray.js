f4679919317a41d21e0c5b6981ad3a67d40f2807
Daniel Zeev <86726359+danielzeev@users.noreply.github.com>
2024-08-16 12:11:04 +0300

    Added solution for execise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(listToProcess, ...removeArgs) {
+    removeArgs = removeArgs.flat(); // Flatten in case an array was inputted   
+    let filteredList = listToProcess;
+    for (const arg of removeArgs) {
+        filteredList = filteredList.filter((listItem) => {return listItem !== arg});
+        }
+    return filteredList;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
