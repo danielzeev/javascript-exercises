c487c8b78dcb3930350931e4e45f080cf7cb1df9
Daniel Zeev <86726359+danielzeev@users.noreply.github.com>
2024-08-16 16:34:42 +0300

    Added solution to exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (!Number.isInteger(year)) return "ERROR";
 
+    let divisibleByFour        = year % 4   == 0;    
+    let divisibleByHundred     = year % 100 == 0;
+    let divisibleByFourHundred = year % 400 == 0;
+
+    if (!divisibleByFour) return false;
+    if (divisibleByHundred && divisibleByFourHundred) return true;
+    if (divisibleByHundred) return false;
+    return true
 };
 
 // Do not edit below this line
